e8b7f1a67f08905bc15cdc8d7d2264a8f2214b02
ruini1 <89319877+ruini1@users.noreply.github.com>
2024-10-29 16:02:41 -0400

    a new start

---
+++ src/server.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    return jsonify({"message": "Hello, world!"})
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    return jsonify({"data": {"id": 1, "name": "Sample Item"}})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# http://127.0.0.1:5000/api/hello
