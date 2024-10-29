14f216cac05a6aafa940ef3467e7d71965b01a18
ruini1 <89319877+ruini1@users.noreply.github.com>
2024-10-29 08:30:54 -0400

    basic framework

--- src/main.lua
+++ src/main.lua
@@ -1,1 +1,22 @@
+local socket = require("http.socket") -- assuming that it works
+local level
+local profile
+local id
 
+
+function send(type)
+  local url = "https://gdbrowser.com/api/"..type"/"..id
+  local response = {}
+
+  local res,code,response_head = http.request{
+    url = url,
+    sink = ltn12.sink.table(response)
+  }
+
+  -- get the actual thing
+  if response = "200" then
+    print("api responded with: " .. table.concat(response))
+  else
+    error("oops" .. code)
+  end
+end
