0a07dc82e8e222c29ccbbe61601e41f34d0ec32a
ruini1 <89319877+ruini1@users.noreply.github.com>
2024-10-29 12:00:00 -0400

    fixes

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-local socket = require("http.socket") -- assuming that it works
+local socket = require("socket.http") -- assuming that it works
 
 print("P or L?")
 local input = string.lower(io.read())
@@ -9,14 +9,14 @@ local id = nil
 if input == "p" then
   method = "profile"
   print("insert id")
-  input = io.read(*number)
+  input = io.read(*n)
   if input ~- nil and input >= 246 then -- still very barebones (lmao)
     send(method, input)
   end
 end
 
 function send(method, id)
-  local url = "https://gdbrowser.com/api/"..method"/"..id
+  local url = "https://gdbrowser.com/api/".. method" ../".. id
   local response = {}
 
   local res,code,response_head = http.request{
@@ -25,7 +25,7 @@ function send(method, id)
   }
 
   -- get the actual thing
-  if response = "200" then
+  if code == 200 then
     local out = print("api responded with: " .. table.concat(response))
     return out -- hopefully this actually works lmao
   else
